0d8c54d213321036e522aded0b0bcfdec035406f
Kai Hoppert <kai.hoppert@tomcom.de>
2013-02-15 15:13:42 +0100

    first commit

---
+++ hoka/browser/base/__init__.py
@@ -0,0 +1,7 @@
+from zope.interface import implements
+from Products.Five import BrowserView as BaseBrowserView
+from zope.interface import Interface
+
+class BrowserView(BaseBrowserView):
+
+    pass
---
+++ hoka/browser/base/tests/test_base.py
@@ -0,0 +1,11 @@
+import unittest
+from hoka.browser.base import BrowserView
+print 'sdfsdfsdf'
+
+class TestBase(unittest.TestCase):
+
+    def test_sample(self):
+        BrowserView(object(),object())
+
+if __name__ == '__main__':
+    unittest.main()
